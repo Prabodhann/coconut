b3f95d6faf4b36ca731f29ab592d0bdf45c83cb0
Prabodhan D. <134509609+Prabodhann@users.noreply.github.com>
2024-05-18 20:57:18 +0530

    verify test 3

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -105,8 +105,8 @@ const placeOrder = async (req, res) => {
     });
 
     const session = await stripe.checkout.sessions.create({
-      success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `https://coconut-frontend.vercel.app/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `https://coconut-frontend.vercel.app//verify?success=false&orderId=${newOrder._id}`,
       line_items: line_items,
       mode: 'payment',
     });
