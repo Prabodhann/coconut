e05d6d1b69d882ea645df1dbd27679963624f8bc
Prabodhan D. <134509609+Prabodhann@users.noreply.github.com>
2024-05-18 15:02:08 +0530

    added token in cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
   })
 );
 
