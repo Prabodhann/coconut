4816aa81165a05d62ab7876b7712b7c305a7f33e
Prabodhan D. <134509609+Prabodhann@users.noreply.github.com>
2024-05-18 14:02:01 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // or specify your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // db connection
 connectDB();
