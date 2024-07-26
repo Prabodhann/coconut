7685ce7aaf383e23507ba7b5bec6764f6f091815
Prabodhan D. <134509609+Prabodhann@users.noreply.github.com>
2024-07-26 13:09:02 +0530

    local host added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import userRouter from './routes/userRoute.js';
-import foodRouter from './routes/foodRoute.js';
-import 'dotenv/config';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
@@ -17,31 +17,32 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'https://coconut-frontend.vercel.app',
-      'https://coconut-frontend-prabodhan-s-projects.vercel.app',
-      'https://coconut-admin.vercel.app',
-      'https://coconut-admin-git-main-prabodhan-s-projects.vercel.app',
+      "https://coconut-frontend.vercel.app",
+      "https://coconut-frontend-prabodhan-s-projects.vercel.app",
+      "https://coconut-admin.vercel.app",
+      "https://coconut-admin-git-main-prabodhan-s-projects.vercel.app",
+      "http://localhost:5173",
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 // Use body-parser to handle JSON payloads and increase limit
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use('/api/user', userRouter);
-app.use('/api/food', foodRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 // Static files (if using file system for images)
-app.use('/images', express.static('uploads'));
+app.use("/images", express.static("uploads"));
 
-app.get('/', (req, res) => {
-  res.send('API Working');
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
 app.listen(port, () =>
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './Footer.css';
-import { assets } from '../../assets/assets';
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
